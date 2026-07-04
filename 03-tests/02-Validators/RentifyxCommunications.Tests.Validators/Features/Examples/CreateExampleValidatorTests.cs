ef19ddd5a42efdb481b1bfd6c8835f3ce9ca9148
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2026-07-04 09:57:23 -0300

    feat(tests): add test project skeletons

---
+++ 03-tests/02-Validators/RentifyxCommunications.Tests.Validators/Features/Examples/CreateExampleValidatorTests.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace RentifyxCommunications.Tests.Validators.Features.Examples;
+
+public sealed class CreateExampleValidatorTests
+{
+    [Fact(Skip = "Not yet implemented")]
+    public Task CreateExample_ValidRequest_ShouldPassValidation() => Task.CompletedTask;
+}
---
+++ 03-tests/03-Handlers/RentifyxCommunications.Tests.Handlers/Features/Examples/DeleteExampleHandlerTests.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace RentifyxCommunications.Tests.Handlers.Features.Examples;
+
+public sealed class DeleteExampleHandlerTests
+{
+    [Fact(Skip = "Not yet implemented")]
+    public Task DeleteExample_ShouldReturnDeleted() => Task.CompletedTask;
+}
